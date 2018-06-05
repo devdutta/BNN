ab1c5980c9aecf9cf25dd3682f775ab0238311be
devdutta <devjothi@gmail.com>
2018-06-05 23:49:54 +0800

    Add files via upload

--- tf_utils.py
+++ tf_utils.py
@@ -1,122 +1,92 @@
-import h5py
 import numpy as np
-import tensorflow as tf
-import math
-
-def load_dataset():
-    train_dataset = h5py.File('datasets/train_signs.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
-
-    test_dataset = h5py.File('datasets/test_signs.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
-
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
-    
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+import os
+import glob
+import cv2
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
 
 
-def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
-    """
-    Creates a list of random minibatches from (X, Y)
-    
-    Arguments:
-    X -- input data, of shape (input size, number of examples)
-    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
-    mini_batch_size - size of the mini-batches, integer
-    seed -- this is only for the purpose of grading, so that you're "random minibatches are the same as ours.
-    
+# Function to join all the training and test images and then associate them with their corresponding labels
+def get_key(path):
+    # seperates the key of an image from the filepath
+    key=path.split(sep=os.sep)[-1]
+    return key
+
+def get_data(paths_img,path_label=None,resize_dim=None):
+    '''reads images from the filepaths, resizes them (if given), and returns them in a numpy array
+    Args:
+        paths_img: image filepaths
+        path_label: pass image label filepaths while processing training data, defaults to None while processing testing data
+        resize_dim: if given, the image is resized to resize_dim x resize_dim (optional)
     Returns:
-    mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
-    """
-    
-    m = X.shape[1]                  # number of training examples
-    mini_batches = []
-    np.random.seed(seed)
-    
-    # Step 1: Shuffle (X, Y)
-    permutation = list(np.random.permutation(m))
-    shuffled_X = X[:, permutation]
-    shuffled_Y = Y[:, permutation].reshape((Y.shape[0],m))
-
-    # Step 2: Partition (shuffled_X, shuffled_Y). Minus the end case.
-    num_complete_minibatches = math.floor(m/mini_batch_size) # number of mini batches of size mini_batch_size in your partitionning
-    for k in range(0, num_complete_minibatches):
-        mini_batch_X = shuffled_X[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
-        mini_batch_Y = shuffled_Y[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
-        mini_batch = (mini_batch_X, mini_batch_Y)
-        mini_batches.append(mini_batch)
-    
-    # Handling the end case (last mini-batch < mini_batch_size)
-    if m % mini_batch_size != 0:
-        mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size : m]
-        mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size : m]
-        mini_batch = (mini_batch_X, mini_batch_Y)
-        mini_batches.append(mini_batch)
-    
-    return mini_batches
-
-def convert_to_one_hot(Y, C):
-    Y = np.eye(C)[Y.reshape(-1)].T
-    return Y
-
-
-def predict(X, parameters):
-    
-    W1 = tf.convert_to_tensor(parameters["W1"])
-    b1 = tf.convert_to_tensor(parameters["b1"])
-    W2 = tf.convert_to_tensor(parameters["W2"])
-    b2 = tf.convert_to_tensor(parameters["b2"])
-    W3 = tf.convert_to_tensor(parameters["W3"])
-    b3 = tf.convert_to_tensor(parameters["b3"])
-    
-    params = {"W1": W1,
-              "b1": b1,
-              "W2": W2,
-              "b2": b2,
-              "W3": W3,
-              "b3": b3}
-    
-    x = tf.placeholder("float", [12288, 1])
-    
-    z3 = forward_propagation_for_predict(x, params)
-    p = tf.argmax(z3)
-    
-    sess = tf.Session()
-    prediction = sess.run(p, feed_dict = {x: X})
+        X: group of images
+        y: categorical true labels
+    '''
+    X=[] # initialize empty list for resized images
+    for i,path in enumerate(paths_img):
+        img=cv2.imread(path,cv2.IMREAD_COLOR) # images loaded in color (BGR)
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB) # cnahging colorspace to RGB
+        if resize_dim is not None:
+            img=cv2.resize(img,(resize_dim,resize_dim),interpolation=cv2.INTER_AREA) # resize image to 28x28
+        X.append(img) # expand image to 28x28x1 and append to the list
+        # display progress
+        if i==len(paths_img)-1:
+            end='\n'
+        else: end='\r'
+        print('processed {}/{}'.format(i+1,len(paths_img)),end=end)
         
-    return prediction
+    X=np.array(X) # tranform list to numpy array
+    if  path_label is None:
+        return X
+    else:
+        df = pd.read_csv(path_label) # read labels
+        df=df.set_index('filename') 
+        y_label=[df.loc[get_key(path)]['digit'] for path in  paths_img] # get the labels corresponding to the images
+        y=to_categorical(y_label,10) # transfrom integer value to categorical variable
+        return X, y
+    
+##Function to show image_group for prediction purposes
+def imshow_group(X,y,y_pred=None,n_per_row=10,phase='processed'):
+    '''helper function to visualize a group of images along with their categorical true labels (y) and prediction probabilities.
+    Args:
+        X: images
+        y: categorical true labels
+        y_pred: predicted class probabilities
+        n_per_row: number of images per row to be plotted
+        phase: If the images are plotted after resizing, pass 'processed' to phase argument. 
+            It will plot the image and its true label. If the image is plotted after prediction 
+            phase, pass predicted class probabilities to y_pred and 'prediction' to the phase argument. 
+            It will plot the image, the true label, and it's top 3 predictions with highest probabilities.
+    '''
+    n_sample=len(X)
+    img_dim=X.shape[1]
+    j=np.ceil(n_sample/n_per_row)
+    fig=plt.figure(figsize=(FIG_WIDTH,HEIGHT_PER_ROW*j))
+    for i,img in enumerate(X):
+        plt.subplot(j,n_per_row,i+1)
+        plt.imshow(img)
+        if phase=='processed':
+            plt.title(np.argmax(y[i]))
+        if phase=='prediction':
+            top_n=3 # top 3 predictions with highest probabilities
+            ind_sorted=np.argsort(y_pred[i])[::-1]
+            h=img_dim+4
+            for k in range(top_n):
+                string='pred: {} ({:.0f}%)\n'.format(ind_sorted[k],y_pred[i,ind_sorted[k]]*100)
+                plt.text(img_dim/2, h, string, horizontalalignment='center',verticalalignment='center')
+                h+=4
+            if y is not None:
+                plt.text(img_dim/2, -4, 'true label: {}'.format(np.argmax(y[i])), 
+                         horizontalalignment='center',verticalalignment='center')
+        plt.axis('off')
+    plt.show()
 
-def forward_propagation_for_predict(X, parameters):
-    """
-    Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
-    
-    Arguments:
-    X -- input dataset placeholder, of shape (input size, number of examples)
-    parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"
-                  the shapes are given in initialize_parameters
-
-    Returns:
-    Z3 -- the output of the last LINEAR unit
-    """
-    
-    # Retrieve the parameters from the dictionary "parameters" 
-    W1 = parameters['W1']
-    b1 = parameters['b1']
-    W2 = parameters['W2']
-    b2 = parameters['b2']
-    W3 = parameters['W3']
-    b3 = parameters['b3'] 
-                                                           # Numpy Equivalents:
-    Z1 = tf.add(tf.matmul(W1, X), b1)                      # Z1 = np.dot(W1, X) + b1
-    A1 = tf.nn.relu(Z1)                                    # A1 = relu(Z1)
-    Z2 = tf.add(tf.matmul(W2, A1), b2)                     # Z2 = np.dot(W2, a1) + b2
-    A2 = tf.nn.relu(Z2)                                    # A2 = relu(Z2)
-    Z3 = tf.add(tf.matmul(W3, A2), b3)                     # Z3 = np.dot(W3,Z2) + b3
-    
-    return Z3
-    
+def create_submission(predictions,keys,path):
+    result = pd.DataFrame(
+        predictions,
+        columns=['label'],
+        index=keys
+        )
+    result.index.name='key'
+    result.to_csv(path, index=True)
